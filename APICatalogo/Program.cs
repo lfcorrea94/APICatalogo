0f8260b57d897ea6dd59806b234c26ffb3e7ee4c
lfcorrea94 <lfcorrea94@gmail.com>
2024-08-03 19:36:33 -0300

    Configurando a string de conexão e registrando o service do context no container DI

--- APICatalogo/Program.cs
+++ APICatalogo/Program.cs
@@ -1,3 +1,6 @@
+using APICatalogo.Context;
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +9,12 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+
+// Obtem a string de conexão
+string mySqlConnection = builder.Configuration.GetConnectionString("DefaultConnection");
+// Instância de builder para definir o provedor, registrar e definir o contexto registrando os serviços no container DI
+builder.Services.AddDbContext<AppDbContext>( options=>options.UseMySql(mySqlConnection,
+                                            ServerVersion.AutoDetect(mySqlConnection)) );
 
 var app = builder.Build();
 
