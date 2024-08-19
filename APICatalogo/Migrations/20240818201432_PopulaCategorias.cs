5f94cddf24ade11008218f75e856ed1c2fba7ad0
lfcorrea94 <lfcorrea94@gmail.com>
2024-08-19 19:04:15 -0300

    Carta table, ProdutosControllers método Get,

---
+++ APICatalogo/Migrations/20240818201432_PopulaCategorias.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace APICatalogo.Migrations
+{
+    /// <inheritdoc />
+    public partial class PopulaCategorias : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder mB)
+        {
+            mB.Sql("Insert into Categorias(Nome, ImagemUrl) Values ('Bebidas','bebidas.jpg')");
+            mB.Sql("Insert into Categorias(Nome, ImagemUrl) Values ('Lanches','lanches.jpg')");
+            mB.Sql("Insert into Categorias(Nome, ImagemUrl) Values ('Sobremesas','sobremesas.jpg')");
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder mB)
+        {
+            mB.Sql("Delete from Categorias");
+        }
+    }
+}
